9b08aaa4d84b41acf00093973ec30483320494ab
Cihan Eran <erancihan@gmail.com>
2023-12-31 23:33:46 +0000

    update auto import behaviour

--- entity_component_system/application/__init__.py
+++ entity_component_system/application/__init__.py
@@ -1,25 +1,34 @@
 import importlib
 import os
 
+from .Application import Application
+
+__all__ = [Application]
+
 
 def auto_import():
-    """Automatically import all modules in the same directory as this file."""
+    """
+    Automatically import all modules in the same directory as this file
+    if they are not imported already.
+    """
     cwd = os.path.dirname(os.path.abspath(__file__))
     for filename in os.listdir(cwd):
         if not filename.endswith(".py") or filename == "__init__.py":
             continue
 
-        # get attributes from module
         module_name = filename[:-3]
-        module = importlib.import_module(f".{module_name}", __package__)
 
+        # check if module is already imported
+        if globals().get(module_name) is not None:
+            continue
+
+        # get attributes from module
+        module = importlib.import_module(f".{module_name}", __package__)
         attrs = [
             getattr(module, name) for name in dir(module) if not name.startswith("_")
         ]
 
         # debug print attrs
-        print(f":: {__file__} {module_name}: {attrs}")
-
         # export attributes to this module
         for attr in attrs:
             globals()[attr.__name__] = attr
