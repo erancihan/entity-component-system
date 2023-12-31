835b4602d8caca0aff5cee004a622aa8e2fd094e
Cihan Eran <erancihan@gmail.com>
2023-12-31 23:09:06 +0000

    update installation script
    require python 3.12
    fix venv activation
    silent poetry installation

--- .devops/install.sh
+++ .devops/install.sh
@@ -1,16 +1,40 @@
+#!/bin/bash
+
+# use python 3.12, check if it exists
+# check if python3.12 is installed
+if ! [ -x "$(command -v python3.12)" ]
+then
+    echo "Python 3.12 could not be found, please install it."
+    exit
+fi
+
+
 ## check if Python venv exists
 if [ ! -d ".venv" ]; then
     echo "Creating Python venv..."
-    python3 -m venv .venv
+    python3.12 -m venv .venv
+fi
+
+# check if venv is created
+if [ ! -d ".venv" ]; then
+    echo "Python venv could not be found, please create it."
+    echo $PWD
+    exit
 fi
 
 ## activate Python venv
 echo "Activating Python venv..."
-source .venv/bin/activate
+. .venv/bin/activate
+
+# check if venv is activated
+if [ ! -n "$VIRTUAL_ENV" ]; then
+    echo "Python venv could not be activated, please activate it."
+    exit
+fi
 
 ## install Python dependencies
 echo "Installing Python dependencies..."
-pip install poetry
+pip install poetry -q
 poetry install
 
 ## activate pre-commit hooks
